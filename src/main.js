3c579ec9e133c507b79cda200f50680238cb16fa
Italo Amaya <italo@Italos-MacBook-Pro.local>
2023-08-22 15:01:50 -0500

    update with some more code

--- src/main.js
+++ src/main.js
@@ -1,18 +1,33 @@
-const { invoke } = window.__TAURI__.tauri;
+const { desktopDir } = window.__TAURI__.path;
 
-let greetInputEl;
-let greetMsgEl;
 
-async function greet() {
-  // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+window.addEventListener("DOMContentLoaded", async () => {
+  // Get the Desktop path
+  const desktopPath = await getDesktopDir();
+  document.querySelector("#cerrar").addEventListener("click"), (e) => {
+    // TODO: add logic to check that filename is not empty
+  }
+});
+
+async function getDesktopDir() {
+  const desktopPath = await desktopDir();
+  document.querySelector("#greet-form").addEventListener("submit", (e) => {
 }
 
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+
+// let greetInputEl;
+// let greetMsgEl;
+
+// async function greet() {
+//   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+//   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+// }
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   greetInputEl = document.querySelector("#greet-input");
+//   greetMsgEl = document.querySelector("#greet-msg");
+//   document.querySelector("#greet-form").addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     greet();
+//   });
+// });
