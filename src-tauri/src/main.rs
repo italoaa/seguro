8fd24fad78c2ad6835cf309b75d57b48d3294130
Italo Amaya <italo@Italos-MacBook-Pro.local>
2023-08-23 18:16:32 -0500

    App ready for encryption backend

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,54 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::fs;
+use tauri::api::path;
+
+// Create a vault
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn create_vault() -> i32 {
+    // Create a Directory in the desktop named 'CajaFuerte'
+    // Match on getting the Desktop Directory
+    match path::desktop_dir() {
+        None => {
+            return 2;
+        }
+        Some(mut desktop_path) => {
+            // Create the directory
+            desktop_path.push("CajaFuerte");
+            match fs::create_dir(desktop_path) {
+                Ok(_) => {
+                    return 0;
+                }
+                Err(_) => {
+                    return 1;
+                }
+            }
+        }
+    }
 }
 
-// #[tauri::command]
-// fn select_dir() -> String {}
+// Close Valut
+#[tauri::command]
+fn close_vault(password: &str) -> i32 {
+    println!("{}", password);
+    return 0;
+}
+
+// Open valut
+#[tauri::command]
+fn open_vault(password: &str) -> i32 {
+    println!("the password is{}", password);
+    return 0;
+}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            create_vault,
+            open_vault,
+            close_vault
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,100 @@
 const { desktopDir } = window.__TAURI__.path;
+const { exists , BaseDirectory} = window.__TAURI__.fs;
+const { invoke } = window.__TAURI__.tauri;
+const { message } = window.__TAURI__.dialog;
 
 
+// Main application logic
 window.addEventListener("DOMContentLoaded", async () => {
-  // Get the Desktop path
-  const desktopPath = await getDesktopDir();
-  document.querySelector("#cerrar").addEventListener("click"), (e) => {
-    // TODO: add logic to check that filename is not empty
+  // Is the valut Present
+  const vaultPath = await isValutPresent();
+
+  // check if the valut exists
+  if (vaultPath) {
+    // The Valut exists!
+    operations();
+  } else {
+    // it does not exist
+    creation();
   }
+
+  let passwordInput;
+
+  // event listeners for clicks on buttons
+  document.querySelector('#crear').addEventListener("click", async () => {
+    const result = await invoke("createVault");
+    // Reload the app
+    if (result === 0) { // 0 is success
+      // Success
+      location.reload()
+    } else { // Something else but 0 so error
+      // handle and notify
+      if (result === 1) { // Could not create the folder
+
+        message("No se pudo crear la caja fuerte", {title: 'Error', type: "error"});
+        location.reload()
+
+      } else if (result === 2) { // Could not get the desktop path
+
+        message("No se pudo conseguir camino al escritorio", {title: 'Error', type: "error"});
+        location.reload()
+
+      }
+    }
+  })
+
+  document.querySelector('#abrir').addEventListener("click", async () => {
+    passwordInput = document.querySelector('#password');
+    if (passwordInput.value === "") {
+      // alert empty password
+      message("Contraseña Vacía", {title: 'Error', type: "error"});
+    } else {
+      const result = await invoke("open_vault", { password : passwordInput.value});
+      console.log("abrir " + result);
+    }
+  })
+
+  document.querySelector('#cerrar').addEventListener("click", async () => {
+    passwordInput = document.querySelector('#password');
+    if (passwordInput.value === "") {
+      // alert empty password
+      message("Contraseña Vacía", {title: 'Error', type: "error"});
+    } else {
+      const result = await invoke("close_vault", { password : passwordInput.value});
+      console.log(result);
+    }
+  })
 });
 
-async function getDesktopDir() {
+async function isValutPresent() {
+  // Get the path to the Desktop
   const desktopPath = await desktopDir();
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+
+  // Check if an opened or closed vault exist
+  const decrypted_vault = await exists('CajaFuerte', {dir : BaseDirectory.Desktop} )
+  const encrypted_vault = await exists('CajaFuerte.encrypted', {dir : BaseDirectory.Desktop} )
+
+  return decrypted_vault || encrypted_vault
 }
+
+function operations() {
+  // Display elements with operations class
+  console.log("display operations!");
+  document.querySelectorAll('.operations').forEach(el => {
+    el.classList.remove("hide");
+  });
+}
+
+function creation() {
+  // Display elements with creation class
+  console.log("display creations!");
+  document.querySelectorAll('.creations').forEach(el => {
+    el.classList.remove("hide");
+    console.log(el);
+  });
+
+}
+
 
 
 // let greetInputEl;
