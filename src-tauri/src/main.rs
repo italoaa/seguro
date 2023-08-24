4ec8c678768ceffefa586668863463ce6770cad5
Italo Amaya <italo@Italos-MacBook-Pro.local>
2023-08-24 12:18:50 -0500

    Refactored everything that has to do with filename

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,8 +7,48 @@ use tauri::api::path;
 
 mod encryption;
 use encryption::{decrypt_file, encrypt_file, unzip_folder, zip_folder};
-
 use zeroize::Zeroize;
+
+const VAULT_NAME: &str = "CajaFuerte";
+
+struct Filenames {
+    vault_path: PathBuf, // The pathBuffer for the original vault
+    zipped: PathBuf,
+    szipped: String,
+    encrypted: String,
+}
+
+fn get_filenames(vault_name: &str) -> Filenames {
+    let desktop: String = path::desktop_dir()
+        .expect("Can't find the desktop")
+        .into_os_string()
+        .into_string()
+        .unwrap();
+
+    // Define names for vault and zipping
+    let mut vault_path: PathBuf = PathBuf::from(desktop);
+    vault_path.push(vault_name);
+
+    // Get the string version due to OS
+    let svault_path: String = vault_path.clone().into_os_string().into_string().unwrap();
+
+    // Clone the main path and set the extension to zip
+    let mut zipped: PathBuf = vault_path.clone();
+    zipped.set_extension("zip");
+
+    // Get the string version of the path buf
+    let szipped: String = zipped.clone().into_os_string().into_string().unwrap();
+
+    // Clone the main
+    let encrypted = svault_path.clone() + ".encrypted";
+
+    return Filenames {
+        vault_path,
+        zipped,
+        szipped,
+        encrypted,
+    };
+}
 
 // Create a vault
 #[tauri::command]
@@ -37,32 +77,22 @@ fn create_vault() -> i32 {
 // Close Valut
 #[tauri::command]
 fn close_vault(mut password: String) -> i32 {
-    let desktop: String = match path::desktop_dir() {
-        None => {
-            panic!("no desktop")
-        }
-        Some(file) => file.into_os_string().into_string().unwrap(),
-    };
-
-    let zip_src = PathBuf::from(desktop.clone() + "/CajaFuerte");
-    let zip_dest = PathBuf::from(desktop.clone() + "/CajaFuerte.zip");
-    let enc_dest = zip_dest
-        .clone()
-        .into_os_string()
-        .into_string()
-        .unwrap()
-        .strip_suffix(".zip")
-        .unwrap()
-        .to_string()
-        + ".encrypted";
-    let enc_src = zip_dest.clone().into_os_string().into_string().unwrap();
-
     // Handle Zip
-    match zip_folder(&zip_src, &zip_dest) {
+    let filenames: Filenames = get_filenames(VAULT_NAME);
+
+    // println!(
+    //     "1. {}\n2. {}\n3. {}",
+    //     filenames.vault_path.into_os_string().into_string().unwrap(),
+    //     filenames.szipped,
+    //     filenames.encrypted
+    // );
+    // return 0;
+
+    match zip_folder(&filenames.vault_path, &filenames.zipped) {
         Err(e) => panic!("{}", e),
         Ok(_) => {
             // handle the encryption
-            match encrypt_file(&enc_src, &enc_dest, &password) {
+            match encrypt_file(&filenames.szipped, &filenames.encrypted, &password) {
                 Err(e) => panic!("{}", e),
                 Ok(_) => {
                     password.zeroize();
@@ -76,24 +106,15 @@ fn close_vault(mut password: String) -> i32 {
 // Open valut
 #[tauri::command]
 fn open_vault(mut password: String) -> i32 {
-    let desktop: String = match path::desktop_dir() {
-        None => {
-            panic!("no desktop")
-        }
-        Some(file) => file.into_os_string().into_string().unwrap(),
-    };
-
-    let dec_src = desktop.clone() + "/CajaFuerte.encrypted";
-    let dec_dest = desktop.clone() + "/CajaFuerte.zip";
-    let zip_src = PathBuf::from(dec_dest.clone());
-    let zip_dest = PathBuf::from(dec_dest.strip_suffix(".zip").unwrap().to_string() + "2");
+    // Get the filenames
+    let filenames: Filenames = get_filenames(VAULT_NAME);
 
     // Handle decryption
-    match decrypt_file(&dec_src, &dec_dest, &password) {
+    match decrypt_file(&filenames.encrypted, &filenames.szipped, &password) {
         Err(e) => panic!("{}", e),
         Ok(_) => {
             // handle the unzip
-            match unzip_folder(&zip_src, &zip_dest) {
+            match unzip_folder(&filenames.zipped, &filenames.vault_path) {
                 Err(e) => panic!("{}", e),
                 Ok(_) => {
                     password.zeroize();
