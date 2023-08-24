62f749a835cb2043288d215ff08037b763ea5e12
Italo Amaya <italo@Italos-MacBook-Pro.local>
2023-08-23 19:32:59 -0500

    Encryption works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,13 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::fs;
+use std::path::PathBuf;
 use tauri::api::path;
+
+mod encryption;
+use encryption::{decrypt_file, encrypt_file, unzip_folder, zip_folder};
+
+use zeroize::Zeroize;
 
 // Create a vault
 #[tauri::command]
@@ -30,12 +36,36 @@ fn create_vault() -> i32 {
 
 // Close Valut
 #[tauri::command]
-fn close_vault(password: &str) -> i32 {
-    println!("{}", password);
+fn close_vault(mut password: String) -> i32 {
+    let desktop: String = match path::desktop_dir() {
+        None => {
+            panic!("no desktop")
+        }
+        Some(file) => file.into_os_string().into_string().unwrap(),
+    };
+
+    let zip_src = PathBuf::from(desktop.clone() + "/CajaFuerte");
+    let zip_dest = PathBuf::from(desktop.clone() + "/CajaFuerte-zip");
+    let zip_dest2 = zip_dest.clone();
+    let _ = zip_folder(&zip_src, &zip_dest);
+
+    let enc_dest = zip_dest
+        .clone()
+        .into_os_string()
+        .into_string()
+        .unwrap()
+        .to_string()
+        + ".encrypted";
+    let enc_src = zip_dest.into_os_string().into_string().unwrap();
+
+    // handle the encryption
+    let _ = encrypt_file(&enc_src, &enc_dest, &password).unwrap();
+
+    password.zeroize();
     return 0;
 }
 
-// Open valut
+// TODO Open valut
 #[tauri::command]
 fn open_vault(password: &str) -> i32 {
     println!("the password is{}", password);
